99652233c7f7a531f756bc12559ce3162c41c7ff
RomanRobotnik <rnavarro@robotnik.es>
2020-12-09 23:18:15 +0100

    Add topic_health_monitor in Python version

--- src/rcomponent/test_node.py
+++ src/rcomponent/test_node.py
@@ -4,16 +4,17 @@ import rospy
 
 from rcomponent import RComponent
 
+
 def main():
 
-	rospy.init_node("rcomponent_test")
+    rospy.init_node("rcomponent_test")
 
-	rc_node = RComponent()
+    rc_node = RComponent()
 
-	rospy.loginfo('%s: starting'%(rospy.get_name()))
+    rospy.loginfo('%s: starting' % (rospy.get_name()))
 
-	rc_node.start()
+    rc_node.start()
 
 
 if __name__ == "__main__":
-	main()
+    main()
